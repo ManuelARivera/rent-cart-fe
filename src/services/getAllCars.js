1e738d6995aad3bf3980319ae30e9a0b79f11878
Manuel A. Rivera <Manuel A. Rivera@DESKTOP-9E0ESCK>
2024-03-04 23:35:36 -0400

    add env var

--- src/services/getAllCars.js
+++ src/services/getAllCars.js
@@ -1,9 +1,11 @@
+const BE_URL = import.meta.env.VITE_BE_BASE_URL
+
 export const getAllCars = async () => {
   const options = {
     method: 'GET',
     headers: { "Content-Type": "application/json" }
   }
-  const response = await fetch('http://localhost:3000/car', options)
-  if(!response.ok) return []
+  const response = await fetch(`${BE_URL}/car`, options)
+  if (!response.ok) return []
   return response.json()
 }
